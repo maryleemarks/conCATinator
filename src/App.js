9e02b2ee25887cd270b7b121cf070b06b53266a4
Mary Marks <maryleemarks@gmail.com>
2022-03-01 19:30:25 -0800

    add initial routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from "./Pages/Home";
+import Login from "./Pages/Login";
+import CreateCat from "./Pages/CreateCat";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/" element={<Login />} />
+        <Route path="/" element={<CreateCat />} />
       </Routes>
     </Router>  
   );
