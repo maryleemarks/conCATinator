1000c2f3b22609004b99cfc94e426af040d8b040
Mary Marks <maryleemarks@gmail.com>
2022-03-02 20:46:46 -0800

    add most of home page layout. There are bugs, but stopping for the day

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,7 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { getDocs, collection } from 'firebase/firestore';
+import { auth, db } from "../firebase";
 
 function Home() {
-  return<div>Home</div>;
+  const [catLists, setCatList] = useState([]);
+  const catsCollectionRef = collection(db, "cats");
+
+  useEffect(() => {
+    const getCats = async () => {
+      const data = await getDocs(catsCollectionRef);
+      setCatList(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    };
+
+    getCats();
+  });
+  return (
+    <div className="homePage">
+    {catLists.map((cat) => {
+      return (
+        <div className="post">
+          <div className="postHeader">
+            <div className="title">
+              <h1> {cat.name}</h1>
+            </div>
+          </div>
+          <div className="postTextContainer"> {cat.notes} </div>
+          <h3>@{cat.author.name}</h3>
+        </div>
+      );
+    })}
+  </div>
+);
 }
 
 export default Home;
