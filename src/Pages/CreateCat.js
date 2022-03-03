84eea3f45ac4efd046e4a47adb816414c12b9a8d
Mary Marks <maryleemarks@gmail.com>
2022-03-02 18:36:18 -0800

    add Create cat functionality

--- src/Pages/CreateCat.js
+++ src/Pages/CreateCat.js
@@ -1,7 +1,21 @@
 import React from "react";
 
 function CreateCat() {
-  return<div>CreateCat</div>;
+  return (
+  <div className='createPostPage'>
+    <div><h1>Add a cat</h1>
+    <div className="inputGp">
+      <label>Name: </label>
+    </div>
+    <div>  
+      <input placeholder="Kitty's Name... "/>
+      <label>Notes: </label>
+      <textarea placeholder="What's up with this kitty?"/>
+    </div>
+  <button>Add Cat</button>
+  </div> 
+</div>  
+  );
 }
 
 export default CreateCat;
